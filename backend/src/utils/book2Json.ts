2d3c892d56ca49e6fb638f33dd7df093242cf861
taizer(PC) <sasha.sachok@gmail.com>
2023-11-12 22:43:45 +0300

    add to fb2 converter

--- backend/src/utils/book2Json.ts
+++ backend/src/utils/book2Json.ts
@@ -1,5 +1,7 @@
 import { readFileSync } from 'fs';
 import iconv from 'iconv-lite';
+import fs from 'fs';
+import { isArray, isEmpty } from 'lodash';
 
 const descriptionTag = 'description';
 const annotaionTag = 'annotation';
@@ -9,6 +11,12 @@ const binaryTag = 'binary';
 const titleTag = 'title';
 const fullLineTag = '<br/>';
 const paragrafTag = 'p';
+const titleInfoTag = 'title-info';
+const documentInfoTag = 'document-info';
+const publishInfoTag = 'publish-info';
+const bookTitleTag = 'book-title';
+const authorTag = 'author';
+const languageTag = 'lang';
 
 const getCodeForEncoding = (file: Buffer) => {
     const stringedFile = file.toString('utf8', 0, 100);
@@ -23,11 +31,33 @@ const getCodeForEncoding = (file: Buffer) => {
     return encodingArray[code];
 };
 
-const getTagsList = (string: string, tag: string) =>  {
+// const getTagsList = (string: string, tag: string) =>  {
+//     const clearString = string.replace(/[\r\n]+/g, "");
+//     const splitedString = clearString.split(`<${tag}`).join('').split(`</${tag}>`)
 
-    const clearString = string.replace(/[\r\n]+/g, "").split(`<${tag}`).join('').split(`</${tag}>`).slice(0,-1);
+//     const result = splitedString.map((item) => {
+//         const str = item.trim();
 
-    const result = clearString.map((item) => {
+//         if (str.charAt(0) === '>'){
+//             return str.slice(1).trim();
+//         }
+
+//         return str;
+//     });
+
+//     // result.shift();
+//     // result.pop();
+
+//     return result;
+// };
+
+const getTagsList = (str: string, tag: string) =>  {  
+    const arr = ['0', str, '0'];
+    const string = arr.join('');
+
+    const splitedString = string.split(`<${tag}`);
+
+    const firstArray = splitedString.map((item) => {
         const str = item.trim();
 
         if (str.charAt(0) === '>'){
@@ -35,6 +65,27 @@ const getTagsList = (string: string, tag: string) =>  {
         }
 
         return str;
+    });
+
+    const result = [] as any;
+
+    firstArray.forEach((item) => {
+        const str = item.trim();
+
+        const splitedStr = str.split(`</${tag}>`);
+
+        if (isArray(splitedStr)) {
+            splitedStr.forEach(item => {
+                const str1 = item.trim();
+
+                if (isEmpty(str1)) {
+                    return;
+                }
+
+                result.push(str1);
+            })
+        } else
+        result.push(splitedStr);
     });
 
     result.shift();
@@ -65,12 +116,14 @@ const getTagValue = (string: string, tag: string) => {
 
 const getSections = (body: string, sectionTag: string) => {
     const sections = getTagsList(body, sectionTag);
-    const result = sections.map((item) => {
+    const result = sections.map((item: string) => {
         const title = getTagContainsString(item, titleTag);
         const titleParagrafs = getTagsList(title, paragrafTag);
-        const paragrafs = getTagsList(item, paragrafTag);
 
-        return {titleParagrafs, paragrafs};
+        const paragrafsString = item.replace(`<${titleTag}>` + title + `</${titleTag}>`,'');
+        const paragrafs = getTagsList(paragrafsString, paragrafTag);
+
+        return {title: titleParagrafs, paragrafs};
     });
     return result;
 }
@@ -81,7 +134,7 @@ const getBinaryData = (book: string, tag: string) => {
     const contenttypeString = 'content-type';
     
 
-    const jsonBinaries = binaries.map((item) => {
+    const jsonBinaries = binaries.map((item: string) => {
         const data = item.split('>');
 
         const binary = data[1];
@@ -96,6 +149,34 @@ const getBinaryData = (book: string, tag: string) => {
     return jsonBinaries;
 }
 
+const writeFileSync = (path: string, base64File: any) =>  {
+    try {
+        const buff = Buffer.from(base64File, 'base64');
+        fs.writeFileSync(path, buff);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getDiscription = (string: string) => {
+    const titleInfoString = getTagContainsString(string, titleInfoTag);
+
+    const bookTitle = getTagsList(titleInfoString, bookTitleTag);
+    const author = getTagsList(titleInfoString, authorTag);
+    
+    const annotation = getTagContainsString(titleInfoString, annotaionTag);
+    const clearAnnotation = getTagsList(annotation, paragrafTag);
+
+    const language = getTagContainsString(titleInfoString, languageTag);
+
+    const documentInfoString = getTagContainsString(string, documentInfoTag);
+    const publisinfoString = getTagContainsString(string, publishInfoTag);
+
+
+
+    return {bookInfo: {bookTitle, annotation: clearAnnotation, author, language}}
+}
+
 export const getBookText = (link: string | null | undefined) => {
     if (!link) {
         return null;
@@ -107,19 +188,26 @@ export const getBookText = (link: string | null | undefined) => {
 
     const stringedBook = iconv.encode(iconv.decode (file, code), 'utf8').toString();
 
+    const clearString = stringedBook.replace(/[\r\n]+/g, "");
+    const clearStringedBook = clearString.replace(/<empty-line\/>/g, "");
+
     // отдельные функции для обработки
-    const description = getTagContainsString(stringedBook, descriptionTag);
-    const body = getTagContainsString(stringedBook, bodyTag);
-    const binaries = getBinaryData(stringedBook, binaryTag);
+    const description = getTagContainsString(clearStringedBook, descriptionTag);
+    const body = getTagContainsString(clearStringedBook, bodyTag);
+    // const binaries = getBinaryData(stringedBook, binaryTag);
 
     // const binary = getTagContainsString(stringedBook, binaryTag);
 
-    const annotation = getTagContainsString(description, annotaionTag);
 
-    // const sections = getTagsList(body, sectionTag);
-    const sections = getSections(body, sectionTag);
+    // const sections = getSections(body, sectionTag);
 
-    const clearAnnotation = getTagsList(annotation, paragrafTag);
 
-    return {sections}
+    //binaries delete first
+
+    // writeFileSync(`storage/${binaries[1].id}`, binaries[1].binary);
+
+    const result = getSections(body, sectionTag);
+    const description1 = getDiscription(description);
+
+    return {result, description1}
 }
